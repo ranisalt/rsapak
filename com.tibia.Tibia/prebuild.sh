d6646fe1d37f6de7fae87662e995e223b44cddce
Ranieri Althoff <ranisalt@gmail.com>
2023-02-12 00:52:54 +0100

    Revert "Redownload Tibia client on build"
    This reverts commit 350fdbdee4cfde71ef0ebde924fb3f3d2d406d4d.

--- com.tibia.Tibia/prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -eu
-
-TARBALL_NAME='tibia.x64.tar.gz'
-curl --compressed --location --referer 'https://www.tibia.com/' --output "$TARBALL_NAME" "https://static.tibia.com/download/$TARBALL_NAME"
-
-TIBIA_TAR_GZ_CHECKSUM=$(sha256sum "$TARBALL_NAME" | cut -c '1-64')
-sed -E "/TIBIA_TAR_GZ_CHECKSUM/ s/[[:xdigit:]]{64}/$TIBIA_TAR_GZ_CHECKSUM/" -i com.tibia.Tibia.yml
